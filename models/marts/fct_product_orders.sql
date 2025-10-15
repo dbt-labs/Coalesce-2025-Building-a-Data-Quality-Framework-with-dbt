9f02660919da0eaba4df252e12e464fd2732215f
emily mohre <emily.mohre@dbtlabsworkshop.com>
2025-10-15 17:46:36 +0000

    session 1

--- models/marts/fct_product_orders.sql
+++ models/marts/fct_product_orders.sql
@@ -1,27 +1,23 @@
 with
 
-orders as (
-    select * from {{ ref('fct_orders') }}
-),
+    orders as (select * from {{ ref("fct_orders") }}),
 
-product_orders as (
-    select * from {{ ref('int_product_orders') }}
-),
+    product_orders as (select * from {{ ref("int_product_orders") }}),
 
-joined as (
-    select
-        product_orders.*,
-        orders.employee,
-        orders.employee_email,
-        orders.store_id,
-        orders.store_name,
-        orders.store_manager,
-        orders.store_manager_email,
-        orders.regional_manager,
-        orders.regional_manager_email
-    from product_orders
-    left join orders
-        on product_orders.order_id = orders.order_id
-)
+    joined as (
+        select
+            product_orders.*,
+            orders.employee,
+            orders.employee_email,
+            orders.store_id,
+            orders.store_name,
+            orders.store_manager,
+            orders.store_manager_email,
+            orders.regional_manager,
+            orders.regional_manager_email
+        from product_orders
+        left join orders on product_orders.order_id = orders.order_id
+    )
 
-select * from joined
+select *
+from joined
