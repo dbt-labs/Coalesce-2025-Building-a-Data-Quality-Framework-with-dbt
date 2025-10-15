c10737865de8f0257df166beb94805b70c0b3f9d
camila birocchi <camila.birocchi@dbtlabsworkshop.com>
2025-10-15 19:15:43 +0000

    exercise 4

--- models/staging/jaffle_world/stg_jaffle_world__customers.sql
+++ models/staging/jaffle_world/stg_jaffle_world__customers.sql
@@ -1,25 +1,25 @@
-with 
+WITH 
 
-source as (
-    select * from {{ source('jaffle_world', 'customers') }}
-),
-
-renamed as (
-    select
-        id as customer_id,
-        address_id,
-        firstname as first_name,
-        lastname as last_name,
-        first_name || ' ' || last_name as full_name,
-        email,
-        regexp_like(
-            email, 
-            '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'
-        ) as is_valid_email,
-        phone,
-        created as created_at,
-        updated as updated_at
-    from source
+source AS (
+    SELECT * FROM {{ source('jaffle_world', 'customers') }}
 )
 
-select * from renamed
+, renamed AS (
+    SELECT
+        id AS customer_id
+        , address_id
+        , firstname AS first_name
+        , lastname AS last_name
+        , first_name || ' ' || last_name AS full_name
+        , email
+        , regexp_like(
+            email
+            , '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'
+        ) AS is_valid_email
+        , phone
+        , created AS created_at
+        , updated AS updated_at
+    FROM source
+)
+
+SELECT * FROM renamed
